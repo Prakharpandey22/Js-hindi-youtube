34495cea7c9f82109d35610d8a9a2254c5c0fe1e
Prakharpandey22 <prakharpandey8898@gmail.com>
2025-10-29 07:31:39 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02" > 1);
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null>=0);       //here null is converted into zero
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+
+
+/// ===  checks strictly the datatypes as well
+
+console.log("2"===2);
+
+
+
