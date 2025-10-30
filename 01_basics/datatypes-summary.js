f154fc5d8c204083c0bf9ab7cf3df2fc1295bf4d
Prakharpandey22 <prakharpandey8898@gmail.com>
2025-10-30 06:22:47 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id===anotherId);
+//console.log(id===anotherId);
 
 //const bigNumber = 3455678877655777777777n
 
@@ -32,4 +32,29 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof myFunction);
+//console.log(typeof myFunction);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(primitive) , Heap(Non-Primitive) 
+
+let myYoutubename ="hiteshchaudhary.com"
+let anothername =myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
